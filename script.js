331c5c7acc59a484951a5da56c572880da234a9f
ailok84 <ailok84@users.noreply.github.com>
2018-04-03 11:12:10 +0200

    pictures added

--- script.js
+++ script.js
@@ -5,10 +5,38 @@ function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function showImages() {
+    document.getElementById('images_menu').style.display = "block";
+    document.getElementById('menu_categories').style.display = "block";
+    water.classList.add("moveWater");
+    nature.classList.add("moveNature");
+    landscape.classList.add("moveLandscape");
+    family.classList.add("moveFamily");
+}
+
+
+function hideImages() {
+    document.getElementById('images_menu').style.display = "none";
+}
+
+
+
+let water = document.querySelector(".water");
+let nature = document.querySelector(".nature");
+let landscape = document.querySelector(".landscape");
+let family = document.querySelector(".family");
+
+
+$('.logo').on("click", function () {
+    $(window).scrollTop(0);
+});
