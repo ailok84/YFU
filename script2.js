f219b62e53dd5df1bd29adb2b91dd97d62267bbe
ailok84 <ailok84@users.noreply.github.com>
2018-04-03 10:48:39 +0200

    updated everything

---
+++ script2.js
@@ -0,0 +1,25 @@
+function showImages() {
+   document.getElementById('images_menu').style.display = "block";
+    document.getElementById('menu_categories').style.display = "block";
+    water.classList.add("moveWater");
+    nature.classList.add("moveNature");
+    landscape.classList.add("moveLandscape");
+    family.classList.add("moveFamily");
+}
+
+
+function hideImages() {
+   document.getElementById('images_menu').style.display = "none";
+}
+
+
+
+let water = document.querySelector(".water");
+let nature = document.querySelector(".nature");
+let landscape =document.querySelector(".landscape");
+let family = document.querySelector(".family");
+
+
+$('.logo').on("click",function(){
+      $(window).scrollTop(0);
+});
